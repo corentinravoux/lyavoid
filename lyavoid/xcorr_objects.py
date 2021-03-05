d18b69e132701be3fe0cb6b26cf5f400b2cfd278
Corentin Ravoux <corentin.ravoux.research@gmail.com>
2021-03-05 16:17:52 +0100

    Implementation of Models for Cross corr

--- lyavoid/xcorr_objects.py
+++ lyavoid/xcorr_objects.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from lyavoid import utils
 import numpy as np
 from scipy.interpolate import griddata
+from scipy import integrate
 
 
 
@@ -48,16 +49,26 @@ class CrossCorr(object):
         return(cls(name=name,mu_array=mu_array,r_array=r_array,xi_array=xi_array,z_array=z_array,exported=exported))
 
 
-    def write(self,xcf_param,weights):
+    def write(self,xcf_param=None):
 
         out = fitsio.FITS(self.name,'rw',clobber=True)
-        head = [ {'name':'RMIN','value':xcf_param["r_min"],'comment':'Minimum r [h^-1 Mpc]'},
-                {'name':'RMAX','value':xcf_param["r_max"],'comment':'Maximum r [h^-1 Mpc]'},
-                {'name':'MUMIN','value':xcf_param["mu_min"],'comment':'Minimum mu = r_para/r'},
-                {'name':'MUMAX','value':xcf_param["mu_max"],'comment':'Maximum mu = r_para/r'},
-                {'name':'NR','value': xcf_param["nbins_r"],'comment':'Number of bins in r'},
-                {'name':'NMU','value':xcf_param["nbins_mu"],'comment':'Number of bins in mu'},
-            ]
+        if(self.exported):
+            nbins_r = self.xi_array.shape[0]
+            nbins_mu = self.xi_array.shape[1]
+        else:
+            nbins_r = self.xi_array.shape[1]
+            nbins_mu = self.xi_array.shape[2]
+        head =[ {'name':'NR','value': nbins_r,'comment':'Number of bins in r'},
+                {'name':'NMU','value': nbins_mu,'comment':'Number of bins in mu'}
+                ]
+        if(xcf_param is not None):
+            head = head + [ {'name':'RMIN','value':xcf_param["r_min"],'comment':'Minimum r [h^-1 Mpc]'},
+                    {'name':'RMAX','value':xcf_param["r_max"],'comment':'Maximum r [h^-1 Mpc]'},
+                    {'name':'MUMIN','value':xcf_param["mu_min"],'comment':'Minimum mu = r_para/r'},
+                    {'name':'MUMAX','value':xcf_param["mu_max"],'comment':'Maximum mu = r_para/r'},
+                    # {'name':'NR','value': xcf_param["nbins_r"],'comment':'Number of bins in r'},
+                    # {'name':'NMU','value':xcf_param["nbins_mu"],'comment':'Number of bins in mu'},
+                ]
         out.write([self.r_array,self.mu_array,self.xi_array,self.z_array],names=['R','MU','DA','Z'],
                 comment=['r','mu = r_para/r','xi','redshift'],
                 units=['h^-1 Mpc','','',''],
@@ -65,7 +76,7 @@ class CrossCorr(object):
         out.close()
 
 
-    def write_no_export(self,xcf_param,weights):
+    def write_no_export(self,xcf_param=None,weights=None):
 
         out = fitsio.FITS(self.name,'rw',clobber=True)
         head = [ {'name':'RMIN','value':xcf_param["r_min"],'comment':'Minimum r [h^-1 Mpc]'},
@@ -128,8 +139,103 @@ class CrossCorr(object):
             self.rmu = True
 
 
-def test_switch(r,mu,xi):
-    corr = CrossCorr(mu_array=mu,r_array=r,xi_array=xi)
-    corr.switch().switch()
-    assert(np.mean(corr.r_array - r) < 10**-10)
-    assert(np.mean(corr.mu_array - mu) < 10**-10)
+# def test_switch(r,mu,xi):
+#     corr = CrossCorr(mu_array=mu,r_array=r,xi_array=xi)
+#     corr.switch().switch()
+#     assert(np.mean(corr.r_array - r) < 10**-10)
+#     assert(np.mean(corr.mu_array - mu) < 10**-10)
+
+
+
+
+
+
+class CrossCorrModel(object):
+
+    def __init__(self,model_name=None,exported=True,supress_first_pixels=0):
+
+        self.model_name = model_name
+        self.exported = exported
+        self.supress_first_pixels = supress_first_pixels
+
+
+
+    def return_model(self,r,mu,real_space_correlation_function):
+        if(self.model_name == "linear"):
+            xcorr_model = SimpleLinearModel()
+        elif(self.model_name == "linear2"):
+            xcorr_model = SimpleLinearModel2()
+        elif(self.model_name == "linear3"):
+            xcorr_model = SimpleLinearModel3()
+        return(xcorr_model.model(r,mu,real_space_correlation_function))
+
+    def compute_model(self,real_space_file,output_name,parameters):
+
+        xcorr = CrossCorr.init_from_fits(real_space_file,exported=True,supress_first_pixels=0)
+
+        r,mu = xcorr.r_array,xcorr.mu_array
+        real_space_correlation_function = xcorr.xi_array
+        model = self.return_model(r,mu,real_space_correlation_function)
+        redshift_space_correlation_function = model(parameters)
+        xcorr.xi_array = redshift_space_correlation_function
+        xcorr.name = output_name
+
+        xcorr.write()
+
+
+
+
+class SimpleLinearModel(object):
+    def __init__(self):
+        self.model_name = "SimpleLinearModel"
+
+    def model(self,r,mu,real_space_correlation_function):
+
+        def model_function(parameters):
+            b = parameters["b"]
+            beta = parameters["beta"]
+            Xi = b**2 * (1 + beta * mu**2)**2 * real_space_correlation_function
+            return(Xi)
+
+        return(model_function)
+
+
+class SimpleLinearModel2(object):
+    def __init__(self):
+        self.model_name = "SimpleLinearModel2"
+
+    def model(self,r,mu,real_space_correlation_function):
+
+        def model_function(parameters):
+            bv = parameters["bv"]
+            ba = parameters["ba"]
+            betav = parameters["betav"]
+            betaa = parameters["betaa"]
+            Xi = bv * ba * (1 + betav * mu**2) * (1 + betaa * mu**2) * real_space_correlation_function
+            return(Xi)
+
+        return(model_function)
+
+
+
+
+class SimpleLinearModel3(object):
+    def __init__(self):
+        self.model_name = "SimpleLinearModel2"
+
+    def model(self,r,mu,real_space_correlation_function):
+
+        Xi_bar = np.zeros(real_space_correlation_function.shape)
+        for i in range(len(r)):
+            mask = (r <= r[i]) & (r > 0)
+            integrand = real_space_correlation_function[mask] * r[mask]**2
+            integral = integrate.simps(integrand,r[mask],axis=0)
+            Xi_bar[i,:] = integral * 3 / (r[i]**3)
+
+        def model_function(parameters):
+            beta = parameters["beta"]
+            Xi = real_space_correlation_function + (beta/3) * Xi_bar + beta*mu**2*(real_space_correlation_function -Xi_bar)
+            return(Xi)
+
+
+        return(model_function)
